3027b8bcb785dec77113bf6b10776bb9e17d1577
Luciaamo <156692681+Luciaamo@users.noreply.github.com>
2024-09-25 14:38:09 +0000

    Añadiendo foto.jpg, fpllefia.jpg y modificando hola.php

--- practicas/pac1-primera-app/hola.php
+++ practicas/pac1-primera-app/hola.php
@@ -1,5 +1,25 @@
-<php>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <style>
+    .foto{
+      width: 100px;
+    }
+  </style>
+</head>
+<body>
   <header>
-    
+    <img src="fpllefia.jpg" alt="fpllefia">
+    <h1>Módulo 7 - Práctica 1. Mi primera aplicación en PHP</h1>
   </header>
-</php>
+
+  <div>
+    <img src="foto.jpg" alt="foto" class="foto">
+    <p></p>
+  </div>
+</body>
+</html>

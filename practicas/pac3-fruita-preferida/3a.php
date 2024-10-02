42d32fe5e1da8f719ca5b6a21e68020c86148ccd
Luciaamo <156692681+Luciaamo@users.noreply.github.com>
2024-10-02 15:48:35 +0000

    3a.php modificada

--- practicas/pac3-fruita-preferida/3a.php
+++ practicas/pac3-fruita-preferida/3a.php
@@ -81,6 +81,29 @@
 
   <main>
 
+  "<div class="col">
+         "<div class="card shadow-sm">
+              "<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
+                "aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">"
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
+                  dy=".3em">Thumbnail</text>
+              </svg>
+
+              "<div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                  content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+            </div>
+          </div>
+        </div>
+      </div>"
+
     <section class="py-5 text-center container">
       <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
@@ -97,208 +120,12 @@
 
     <div class="album py-5 bg-light">
       <div class="container">
+<?php 
+  
+      
+?>
 
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
 
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" role="img" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
-
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -316,28 +143,63 @@
     </div>
   </footer>
 
-
-<?php foreach ($idols as $idol): ?>
-    <div class="col-md-4">
-      <div class="card mb-4">
-          <img src="<?php echo $idol['imatge']; ?>" class="card-img-top" alt="<?php echo $idol['nom']; ?>">
-          <div class="card-body">
-              <h5 class="btn btn-sm btn-outline-secondary"><?php echo $idol['nom']; ?></h5>
-              <p class="card-text"><?php echo $idol['descripcio']; ?></p>
-          </div>
-      </div>
-    </div> 
-<?php endforeach; ?>
-
-
-
-
-
 $idols = [
+  [
         "imatge" => "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/BillieEilishO2160622_%2819_of_45%29_%2852153214339%29_%28cropped_3%29.jpg/640px-BillieEilishO2160622_%2819_of_45%29_%2852153214339%29_%28cropped_3%29.jpg",
         "nom_cognoms" => "Billie Eilish",
         "descripcio" => "Billie Eilish és la meva ídola per diverses raons que la fan destacar al món de la música i la cultura pop. La seva autenticitat i vulnerabilitat a les lletres de les seves cançons ressonen profundament, tocant temes com la salut mental, l'autoacceptació i les experiències personals. A més, el seu estil únic i la seva estètica innovadora la diferencien d'altres artistes i la converteixen en un referent de la generació jove."
-]
+  ],
+  [
+        "imatge" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fopen.spotify.com%2Fintl-es%2Fartist%2F5L1lO4eRHmJ7a0Q6csE5cT&psig=AOvVaw2TCafaVSpleLUQwuFIri6Y&ust=1727964190978000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCKr7ru74gDFQAAAAAdAAAAABAE",
+        "nom_cognoms" => "Lisa",
+        "descripcio" => "Lisa de BLACKPINK es la meva ídola per la seva increïble dedicació i talent en múltiples àmbits. Com a ballarina, és reconeguda per les seves habilitats impecables i precisió. A més, la seva versatilitat com a rapera i cantant ha fet que destaqui dins de la indústria musical."
+  ],
+  [
+        "imatge" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.soompi.com%2Fes%2Farticle%2F501889wpp%2Fjisoo-de-blackpink-brilla-en-nuevas-imagenes-de-perfil-para-su-carrera-como-actor&psig=AOvVaw16Wl16d_XE98qkJ2tCEFKA&ust=1727964433273000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDb4qTv74gDFQAAAAAdAAAAABAE",
+        "nom_cognoms" => "Jisoo",
+        "descripcio" => "Jisoo de BLACKPINK, es la meva ídola per la seva gran dedicació, talent i autenticitat. Com a cantant i actriu, ha demostrat una impressionant versatilitat, tant en l'escenari com fora d'ell. És coneguda per la seva humilitat, treball dur i lideratge tranquil, cosa que la converteix en un exemple a seguir per la seva perseverança i compromís amb els seus somnis."
+  ],
+  [
+        "imatge" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bigfm.de%2Fnachrichten%2Fmusiknews%2Fburn-out-jennie-beendet-blackpink-show-abrupt&psig=AOvVaw2BggCgllKvDwS1C-G8QrXk&ust=1727964574209000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOiuou7v74gDFQAAAAAdAAAAABAJ",
+        "nom_cognoms" => "Jennie",
+        "descripcio" => "Jennie de BLACKPINK, es la meva ídola per la seva gran versatilitat com a artista. És coneguda pel seu talent tant en el cant com en el rap, a més de la seva presència escènica única. També és una icona de la moda, amb un estil influent que ha inspirat moltes persones arreu del món."
+  ],
+  [
+        "imatge" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwwd.com%2Faccessories-news%2Fjewelry%2Ftiffany-rose-blackpink-k-pop-lock-collection-rose-gold-pink-sapphire-1235805341%2F&psig=AOvVaw3tLwmuR7ZArSOK89YSGSq5&ust=1727964689246000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjv0afw74gDFQAAAAAdAAAAABAE",
+        "nom_cognoms" => "Rosé",
+        "descripcio" => "Rosé de BLACKPINK, es la meva ídola per la seva gran dedicació, talent i autenticitat. És coneguda per la seva veu única, habilitat musical i carisma escènic. A més, és una artista molt versàtil, capaç de tocar instruments, ballar i cantar en diversos idiomes. El seu treball dur i la seva passió per la música inspiren a molts a seguir els seus somnis sense rendir-se."
+  ],
+  [
+        "imatge" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FArchivo%3AAva_Max_-_Untold_2023_%252853113549967%2529_%2528cropped%2529.jpg&psig=AOvVaw2_SMuw2eH9zl6rvbRYqe_j&ust=1727964822102000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCtsefw74gDFQAAAAAdAAAAABAK",
+        "nom_cognoms" => "Ava Max",
+        "descripcio" => "Ava Max, es la meva ídola perquè és una artista amb un estil únic i una veu poderosa que destaca en l'escena del pop. Ha aconseguit èxits internacionals amb cançons com "Sweet but Psycho" i "Kings & Queens", transmetent missatges de confiança, empoderament i autenticitat."
+  ],
+  [
+        "imatge" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbc.com%2Fmundo%2Fnoticias-45344939&psig=AOvVaw3wPK71L-AdeeV8MPyegt85&ust=1727965192317000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOjIy4_y74gDFQAAAAAdAAAAABAE",
+        "nom_cognoms" => "Michael Jackson",
+        "descripcio" => "per la seva gran influència en la música, el ball i la cultura pop. Amb una carrera impressionant des de petit, va revolucionar la indústria amb el seu estil únic, cançons icòniques com "Thriller" i coreografies inoblidables."
+  ],
+  [
+        "imatge" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fplus.cusica.com%2F2017%2F12%2F15%2Fescucha-nuevo-disco-eminem%2F&psig=AOvVaw1dgy0yVHrjlBYlvl7nePt2&ust=1727965033128000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjt68fx74gDFQAAAAAdAAAAABAE",
+        "nom_cognoms" => "Eminem",
+        "descripcio" => "Eminem, es el meva ídol per la seva trajectòria impressionant en el món de la música, superant obstacles personals com la pobresa i les addiccions. És conegut per la seva habilitat lírica, la seva autenticitat i la seva capacitat per expressar emocions crues i complexes a través de les seves cançons."
+  ],
+  [
+        "imatge" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fplus.cusica.com%2F2020%2F05%2F04%2Fsia-estrena-su-nuevo-sencillo-saved-my-life-escrito-por-dua-lipa%2Fsia-6%2F&psig=AOvVaw15AfuLHDeEb6J_sJBlaJBJ&ust=1727965409297000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCn5YHz74gDFQAAAAAdAAAAABAE",
+        "nom_cognoms" => "Sia",
+        "descripcio" => "Sia es la meva ídola per la seva veu poderosa i única, combinada amb les seves lletres profundes i emocionals, la converteixen en una artista excepcional. A més, la seva autenticitat i vulnerabilitat en compartir les seves experiències de vida, incloent la seva lluita contra l'ansietat i la depressió, inspiren molts a ser més oberts i acceptar les seves pròpies lluites."
+  ]
+  ];
+
+  <?php
+    foreach($idols as $idol){
+
+    }
+  ?>
+
+
+
+
 
 </body>
 

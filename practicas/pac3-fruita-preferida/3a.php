22bb90b88695935ddb951e34299ba7b83b62229d
Luciaamo <156692681+Luciaamo@users.noreply.github.com>
2024-10-03 18:48:18 +0000

    3a.php modificado

--- practicas/pac3-fruita-preferida/3a.php
+++ practicas/pac3-fruita-preferida/3a.php
@@ -198,7 +198,7 @@ echo '<div class="album py-5 bg-light">
   </footer>
  
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>  
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>  
 
 </body>
 </html>

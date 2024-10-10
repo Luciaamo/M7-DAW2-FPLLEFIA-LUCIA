2667bd54f462c3d9ef8efccb81016ca3a5103f71
Luciaamo <156692681+Luciaamo@users.noreply.github.com>
2024-10-10 16:14:39 +0000

    Nuevos ficheros php creados

---
+++ teoria/components/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <h1>Este es el footer</h1>
+    <p>Copyright fpllefia</p>
+</footer>
---
+++ teoria/index1-include.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PELICULAS 1</title>
+</head>
+
+
+
+<body>
+    <?php
+        include "./components/header.php"
+    ?>
+    <main>
+        <ul>
+            <li><a href="index2-include.php">Ir a pelis 2</a></li>
+            <li><a href="index1-include.php">Ir a pelis 1</a></li>
+        </ul>
+    </main>
+
+    <?php
+        include "./components/footer.php";
+    ?>
+</body>
+</html>

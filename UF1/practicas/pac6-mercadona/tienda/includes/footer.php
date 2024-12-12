0e795be7160276cd3e15bd92bc4e74a309eedfcd
Luciaamo <156692681+Luciaamo@users.noreply.github.com>
2024-12-12 16:40:27 +0000

    Moviendo archivos

--- UF1/practicas/pac6-mercadona/tienda/includes/footer.php
+++
@@ -1,4 +0,0 @@
-
-<footer class="bg-light text-center py-3 mt-5">
-    <p>&copy; 2024 Mercadona - Todos los derechos reservados.</p>
-</footer>
--- UF1/teoria/components/header.php
+++
@@ -1,3 +0,0 @@
-<header>
-        <h1>Películas de mi ciudad</h1>
-</header>
